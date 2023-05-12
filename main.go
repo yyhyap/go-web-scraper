3df84e52c2a9b5b92e53c145def2862ce81fa409
yyhyap <yongheng98@hotmail.com>
2023-05-12 16:30:58 +0800

    Add user agent header

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ func main() {
 
 	c := colly.NewCollector()
 
+	// setting a valid User-Agent header
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+
 	c.OnRequest(func(r *colly.Request) {
 		logger.Logger.Info(fmt.Sprintf("Visiting: %v", r.URL))
 	})
